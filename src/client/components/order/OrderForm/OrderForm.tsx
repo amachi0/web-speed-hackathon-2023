ec9f38fa10fb1c71a6777e9bf8c684d1b7230e8b
天知 <shunsuke.kmkz0617@gmail.com>
2023-03-10 10:13:35 +0900

    zipcode-jaをapiに変更

--- src/client/components/order/OrderForm/OrderForm.tsx
+++ src/client/components/order/OrderForm/OrderForm.tsx
@@ -1,7 +1,5 @@
 import { useFormik } from 'formik';
-import _ from 'lodash';
 import type { ChangeEventHandler, FC } from 'react';
-import zipcodeJa from 'zipcode-ja';
 
 import { PrimaryButton } from '../../foundation/PrimaryButton';
 import { TextInput } from '../../foundation/TextInput';
@@ -30,16 +28,20 @@ export const OrderForm: FC<Props> = ({ onSubmit }) => {
     onSubmit,
   });
 
-  const handleZipcodeChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+  const handleZipcodeChange: ChangeEventHandler<HTMLInputElement> = async ( event) => {
     formik.handleChange(event);
 
     const zipCode = event.target.value;
-    const address = [...(_.cloneDeep(zipcodeJa)[zipCode]?.address ?? [])];
-    const prefecture = address.shift();
-    const city = address.join(' ');
-
-    formik.setFieldValue('prefecture', prefecture);
-    formik.setFieldValue('city', city);
+    if (zipCode.length == 7) {
+      const result = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=${zipCode}`);
+      const jsonData = await result.json();
+      if (jsonData.status !== 200 || jsonData.results.length === 0) {
+        const prefecture = jsonData.results[0].address1;
+        const city = jsonData.results[0].address2;
+        await formik.setFieldValue('prefecture', prefecture);
+        await formik.setFieldValue('city', city);
+      }
+    }
   };
 
   return (
--- src/client/utils/apollo_client.ts
+++ src/client/utils/apollo_client.ts
@@ -2,6 +2,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   cache: new InMemoryCache(),
-  ssrMode: true,
-  uri: '/graphql',
+  uri: '/graphql'
 });
